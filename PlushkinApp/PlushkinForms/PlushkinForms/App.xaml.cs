cbe2107e9975b69b2accf553ab0a63d74dc59fe6
buzzon <buzzondev@gmail.com>
2021-04-22 08:36:22 +0500

    go

---
+++ PlushkinApp/PlushkinForms/PlushkinForms/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using PlushkinForms.Services;
+using PlushkinForms.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PlushkinForms
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        async protected override void OnStart()
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
