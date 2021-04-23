15aff903ed9f0a045c31dda7bd6452628bc76a9e
buzzon <buzzondev@gmail.com>
2021-04-23 10:46:58 +0500

    work validation

--- PlushkinApp/PlushkinForms/PlushkinForms.Android/obj/Debug/110/android/environment.x86_64.s
+++ PlushkinApp/PlushkinForms/PlushkinForms.Android/obj/Debug/110/android/environment.x86_64.s
@@ -72,7 +72,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"4f622cc1-6a2c-487a-bfa8-582b3f3c0910"
+	.asciz	"b930734d-4bd0-4c94-adef-88d472e18633"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
